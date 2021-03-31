a8c19b93879c914715c2a8554f60aa62214bb162
Arnaud <arnaud.rabhi@limayrac.fr>
2021-03-31 10:33:16 +0200

    activity s

--- app/src/main/java/com/example/fredi_android/LoginActivity.java
+++ app/src/main/java/com/example/fredi_android/LoginActivity.java
@@ -3,6 +3,16 @@ package com.example.fredi_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +21,45 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void Connexion() throws MalformedURLException {
+        EditText username = (EditText)findViewById(R.id.userLogin);
+        EditText password = (EditText)findViewById(R.id.mdpLogin);
+
+        JSONObject json = new JSONObject();
+
+        String url_string = "http://localhost/projets/fredi/note_json.php?" + "email=" + username + "&password=" + password;
+
+
+
+        try {
+            URL url = new URL(url_string);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestProperty("User-Agent", "");
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        URL url = new URL("http://localhost/projets/fredi/note_json.php");
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
