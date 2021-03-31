ddeaee2ef31e8a5690c7bd967ca9339f0ae4f3a7
Arnaud <arnaud.rabhi@limayrac.fr>
2021-03-31 09:12:02 +0200

    activity s

---
+++ app/src/main/java/com/example/fredi_android/MyAsyncTask.java
@@ -0,0 +1,14 @@
+package com.example.fredi_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyAsyncTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_async_task);
+    }
+}
